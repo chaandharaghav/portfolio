12f79523fc468502746be3621a11c97b2a3bfcfa
Chaandha Raghav C <chaandharaghav@gmail.com>
2023-11-26 12:08:52 +0530

    Add responsive design improvements

--- components/project/index.tsx
+++ components/project/index.tsx
@@ -28,11 +28,11 @@ const Project = ({ project }: { project: project }) => {
     return (
         <div className="w-full p-6 mb-6 bg-white border-t-4 rounded-sm dark:bg-dark_project_bg dark:border-t-8 dark:border-t-gray-500 border-t-gray-300 dark:text-black">
             <h5 className="font-bold text-md ">{name}</h5>
-            <p className="text-sm underline hover:cursor-pointer ">
+            <p className="text-sm underline break-words hover:cursor-pointer">
                 <Link href={link}>{link}</Link>
             </p>
             <p className="mt-5 text-sm">{description}</p>
-            <div className="flex flex-wrap mt-6">
+            <div className="flex flex-wrap mt-2 lg:mt-6">
                 <Pill content={capitalize(status)} isStatus />
                 {techstack.map((tech, idx) => (
                     <Pill key={idx} content={tech} isStatus={false} />
@@ -62,7 +62,7 @@ const Pill = ({
         );
     }
     return (
-        <div className="px-2.5 py-0.5 rounded-2xl border mr-3">
+        <div className="px-2.5 py-0.5 rounded-2xl border dark:border-gray-600 mr-3 mt-2">
             {isStatus && statusComp}
             <span className="text-sm">{content}</span>
         </div>
--- components/skills/index.tsx
+++ components/skills/index.tsx
@@ -4,9 +4,9 @@ import SkillIcon from "@/components/skills/Skillcon";
 
 const Skills = ({ skills }: { skills: string[] }) => {
     return (
-        <div>
-            <h4 className="mb-2 text-lg font-bold lg:mb-6">Skills</h4>
-            <ul className="flex sm:flex-row">
+        <div className="mb-5">
+            <h4 className="text-lg font-bold lg:mb-6">Skills</h4>
+            <ul className="grid grid-cols-2 min-[400px]:grid-cols-3 sm:flex-row">
                 {skills.map((skill, idx) => (
                     <Skill key={idx} name={skill} />
                 ))}
@@ -17,7 +17,7 @@ const Skills = ({ skills }: { skills: string[] }) => {
 
 const Skill = ({ name }: { name: string }) => {
     return (
-        <li className="m-4 lg:m-0 rounded-full flex w-20 lg:w-32 [&:nth-child(1)]:text-blue-500 [&:nth-child(2)]:text-pink-500 [&:nth-child(3)]:text-orange-500 ">
+        <li className="mx-4 lg:m-0 rounded-full flex w-16 md:w-20 lg:w-32 [&:nth-child(1)]:text-blue-500 [&:nth-child(2)]:text-pink-500 [&:nth-child(3)]:text-orange-500 ">
             <span className="min-w-max flex justify-center p-1.5 m-3 items-center border border-solid border-[currentColor] rounded-full">
                 <SkillIcon className="w-20" />
                 <span className="absolute text-black dark:text-gray-50">
